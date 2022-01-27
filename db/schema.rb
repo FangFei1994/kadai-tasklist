44d32d2fc337774c6ea75536b206ac778f063ac5
FangFei1994 <a13061121616@gmail.com>
2022-01-27 06:22:12 +0000

    kadai-tasklist-add

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_091905) do
+ActiveRecord::Schema.define(version: 2022_01_27_055441) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "status"
   end
 
 end
